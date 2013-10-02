e924b06034be252843108231e34a98c267dc847e
Shana Silas <chefshana@me.com>
2013-10-02 10:57:34 -0400

    fixed bootstrap on heroku

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module Railsforstartups
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+     # Required to add for Devise - On config/application.rb forcing your application to not access the DB or load models when precompiling your assets
+    config.assets.initialize_on_precompile = false
+    
   end
 end
